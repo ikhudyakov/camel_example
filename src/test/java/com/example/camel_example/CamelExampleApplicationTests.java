468a2f545c4e2b92997fd2c55379183e27418d97
”Ilya <Ilya.Khudyakov@innostage-group.ru>
2023-06-06 10:23:29 +0300

    Init commit

---
+++ src/test/java/com/example/camel_example/CamelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camel_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
